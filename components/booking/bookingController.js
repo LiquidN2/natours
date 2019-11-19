b04e36b823011b5b7d96c57b059365ae3b8cf2ba
Hugh Nguyen <hhnguyen255@gmail.com>
2019-11-19 15:00:56 +1100

    minor code cleanup

--- components/booking/bookingController.js
+++ components/booking/bookingController.js
@@ -14,14 +14,17 @@ const getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1. Get tour
   const tour = await Tour.findById(req.params.tourId);
 
-  const port = process.env.NODE_ENV === 'development' ? ':3000' : '';
+  const host =
+    process.env.NODE_ENV === 'production'
+      ? req.get('host')
+      : `${req.host}:${process.env.PORT}`;
 
   // 2. Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     // success_url: `${req.protocol}://${req.host}${port}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}&startDate=${req.params.dateId}`,
-    success_url: `${req.protocol}://${req.get('host')}/my-bookings`,
-    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    success_url: `${req.protocol}://${host}/my-bookings`,
+    cancel_url: `${req.protocol}://${host}/tour/${tour.slug}`,
     customer_email: req.user.email,
     // client_reference_id: req.params.tourId,
     client_reference_id: `${req.params.tourId}_${req.params.dateId}`,
@@ -29,9 +32,7 @@ const getCheckoutSession = catchAsync(async (req, res, next) => {
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        images: [
-          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
-        ],
+        images: [`${req.protocol}://${host}/img/tours/${tour.imageCover}`],
         amount: tour.price * 100, // price in cents
         currency: 'aud',
         quantity: 1
@@ -59,11 +60,7 @@ const getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = async session => {
-  // const idArr = session.client_reference_id.split('_');
-  // const tour = idArr[0];
-  // const startDate = idArr[1];
   const [tour, startDate] = session.client_reference_id.split('_');
-  // const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = (session.display_items[0].amount * 1) / 100;
   await Booking.create({ tour, user, price, startDate });
