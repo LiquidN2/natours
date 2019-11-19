79f5af2630a09075c49b4ed001f87b61363250ee
Hugh Nguyen <hhnguyen255@gmail.com>
2019-11-19 14:50:45 +1100

    Fixed image url on Stripe session. Changed line_items to display_items

--- components/booking/bookingController.js
+++ components/booking/bookingController.js
@@ -20,8 +20,8 @@ const getCheckoutSession = catchAsync(async (req, res, next) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     // success_url: `${req.protocol}://${req.host}${port}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}&startDate=${req.params.dateId}`,
-    success_url: `${req.protocol}://${req.host}${port}/my-bookings`,
-    cancel_url: `${req.protocol}://${req.host}${port}/tour/${tour.slug}`,
+    success_url: `${req.protocol}://${req.get('host')}/my-bookings`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     // client_reference_id: req.params.tourId,
     client_reference_id: `${req.params.tourId}_${req.params.dateId}`,
@@ -29,7 +29,9 @@ const getCheckoutSession = catchAsync(async (req, res, next) => {
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        images: [
+          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
+        ],
         amount: tour.price * 100, // price in cents
         currency: 'aud',
         quantity: 1
@@ -63,7 +65,7 @@ const createBookingCheckout = async session => {
   const [tour, startDate] = session.client_reference_id.split('_');
   // const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = (session.line_items[0].amount * 1) / 100;
+  const price = (session.display_items[0].amount * 1) / 100;
   await Booking.create({ tour, user, price, startDate });
 };
 
